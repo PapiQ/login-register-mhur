a8b5ca308a625ed011fa0de53e925ff05bfbe6db
PapiQ <ananiates92@gmail.com>
2025-02-09 22:05:11 +0300

    removed comment from app.js

--- src/App.js
+++ src/App.js
@@ -13,55 +13,6 @@ function App() {
 
   return (
     <Router>
-      {/*  <div className="App">
-        <div>
-          <img className="logo" src={logo} alt="Logo image" />
-        </div>
-
-        <div>
-          <h1>Welcome to mhur</h1>
-          <ul className="segmented-control">
-            <li className="segmented-control__item">
-              <input
-                className="segmented-control__input"
-                type="radio"
-                value="1"
-                name="option"
-                id="option-1"
-                checked={1 === selectedIndex}
-              />
-              <Link
-                onClick={() => {
-                  setSelectedIndex(1);
-                }}
-                class="segmented-control__label"
-                to="/"
-              >
-                Login
-              </Link>
-            </li>
-            <li className="segmented-control__item">
-              <input
-                className="segmented-control__input"
-                type="radio"
-                value="2"
-                name="option"
-                id="option-2"
-                checked={2 === selectedIndex}
-              />
-              <Link
-                onClick={() => {
-                  setSelectedIndex(2);
-                }}
-                class="segmented-control__label"
-                to="/register"
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
-          <p>We provide the tools and courses to help you succeed.</p>
-          <p>Unlock knowledge and skills with mhur</p> */}
       <Routes>
         <Route exact="true" path="/" element={<Home />}></Route>
         <Route exact="true" path="/home" element={<Home />}></Route>
